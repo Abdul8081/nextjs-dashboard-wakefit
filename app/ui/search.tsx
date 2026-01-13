70952c10e5fede8de751a9d85674af976a1f0aea
Abdul Muid <amuid677@gmail.com>
2026-01-13 18:44:28 +0530

    Added Search and Pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,27 +1,23 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { useSearchParams } from "next/navigation";
-import { useRouter, usePathname } from "next/navigation";
-import {useDebouncedCallback} from 'use-debounce';
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  
+
   const pathname = usePathname();
   const { replace } = useRouter();
-  
   const handleSearch = useDebouncedCallback((term) => {
-
-    console.log(`Searching... ${term}`);
-        const params = new URLSearchParams(searchParams);
-
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
     if (term) {
       params.set("query", term);
     } else {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
-  },300);
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
